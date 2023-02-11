2eb52fe4e1198dc27b8f2e1bed7c29bc36da486b
polinaDenischenk <115966933+polinaDenischenk@users.noreply.github.com>
2023-02-11 15:51:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('*** Задание 1 ***');
+
+let elemen = document.querySelector('#elem');
+elemen.style.color = "red";
+console.log(elem.firstElementChild);
+
+console.log('*** задание 2 ***');
+
+const body1 = document.body;
+console.log(elemen.lastChild);
+
+console.log('*** задание 3 ***');
+
+const body = document.querySelector('#elem');
+elemen.append('!!!');
